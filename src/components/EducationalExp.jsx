519d01788ade267ace568adaf818b529604cf5c6
Maddie <115709272+Maddily@users.noreply.github.com>
2024-12-07 10:52:03 +0200

    Update onChange function call
    The function now takes index of experience as argument

--- src/components/EducationalExp.jsx
+++ src/components/EducationalExp.jsx
@@ -1,33 +1,34 @@
-import Input from "./Input";
-import DataField from "./DataField";
+import Input from './Input';
+import DataField from './DataField';
 
 export default function EducationalExp({
+  index,
   isEditingEducationalExp,
   educationalExpHandler,
   educationalExp,
 }) {
   if (isEditingEducationalExp) {
     return (
-      <div className="experience">
+      <div className="experience" data-index={index}>
         <Input
           type="text"
           label="school name"
           placeholder="e.g., Harvard University"
-          onChange={educationalExpHandler}
+          onChange={(e) => educationalExpHandler(e, index)}
           data={educationalExp}
         />
         <Input
           type="text"
           label="title of study"
           placeholder="e.g., B.Sc. Computer Science"
-          onChange={educationalExpHandler}
+          onChange={(e) => educationalExpHandler(e, index)}
           data={educationalExp}
         />
         <Input
           type="date"
           label="date of study"
           placeholder="e.g., 2018-2022"
-          onChange={educationalExpHandler}
+          onChange={(e) => educationalExpHandler(e, index)}
           data={educationalExp}
         />
       </div>
