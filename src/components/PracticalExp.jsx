54a38f1945be9f8f292498e59ec1f31352daaf4f
Maddie <115709272+Maddily@users.noreply.github.com>
2024-12-07 11:00:02 +0200

    Update onChange function call
    The function now takes index of experience as argument

--- src/components/PracticalExp.jsx
+++ src/components/PracticalExp.jsx
@@ -1,27 +1,28 @@
 import Input from './Input';
 import DataField from './DataField';
-import '../styles/PracticalExp.css'
+import '../styles/PracticalExp.css';
 
 export default function PracticalExp({
+  index,
   isEditingPracticalExp,
   practicalExpHandler,
   practicalExp,
 }) {
   if (isEditingPracticalExp) {
     return (
-      <div className="experience">
+      <div className="experience" data-index={index}>
         <Input
           type="text"
           label="company name"
           placeholder="e.g., TechCorp"
-          onChange={practicalExpHandler}
+          onChange={(e) => practicalExpHandler(e, index)}
           data={practicalExp}
         />
         <Input
           type="text"
           label="position title"
           placeholder="e.g., Software Engineer"
-          onChange={practicalExpHandler}
+          onChange={(e) => practicalExpHandler(e, index)}
           data={practicalExp}
         />
         <div className="field">
@@ -32,21 +33,21 @@ export default function PracticalExp({
             name="responsibilities"
             id="responsibilities"
             placeholder="e.g., Developed web apps"
-            onChange={practicalExpHandler}
+            onChange={(e) => practicalExpHandler(e, index)}
           ></textarea>
         </div>
         <Input
           type="number"
           label="from"
           placeholder="e.g., 2020"
-          onChange={practicalExpHandler}
+          onChange={(e) => practicalExpHandler(e, index)}
           data={practicalExp}
         />
         <Input
           type="number"
           label="to"
           placeholder="e.g., 2023"
-          onChange={practicalExpHandler}
+          onChange={(e) => practicalExpHandler(e, index)}
           data={practicalExp}
         />
       </div>
@@ -57,7 +58,10 @@ export default function PracticalExp({
     <div className="experience">
       <DataField label="company name" data={practicalExp.company} />
       <DataField label="position title" data={practicalExp.position} />
-      <DataField label="main responsibilities" data={practicalExp.responsibilities} />
+      <DataField
+        label="main responsibilities"
+        data={practicalExp.responsibilities}
+      />
       <DataField label="from" data={practicalExp.from} />
       <DataField label="to" data={practicalExp.to} />
     </div>
