1aaa39607e8ef611f726ba7f250c6c4c28fe2725
Maddie <115709272+Maddily@users.noreply.github.com>
2024-12-07 10:56:25 +0200

    Create add button with its own handler
    When clicked, an experience is added to the
    fieldset by the handler.

--- src/components/Fieldset.jsx
+++ src/components/Fieldset.jsx
@@ -1,8 +1,18 @@
 import { useEffect, useState } from 'react';
+import Icon from '@mdi/react';
+import { mdiPlusBox } from '@mdi/js';
 import Button from './Button';
 import '../styles/Fieldset.css';
 
-export default function Fieldset({ legend, children, filled, isEditing, setIsEditingCaller }) {
+export default function Fieldset({
+  legend,
+  children,
+  filled,
+  isEditing,
+  setIsEditingCaller,
+  addEducationalExpHandler,
+  addPracticalExpHandler,
+}) {
   const [editButtonDisabled, setEditButtonDisabled] = useState(true);
   const [submitButtonDisabled, setSubmitButtonDisabled] = useState(true);
 
@@ -20,13 +30,64 @@ export default function Fieldset({ legend, children, filled, isEditing, setIsEdi
     }
   }, [isEditing, filled]);
 
+  const displayAddButton = () => {
+    if (legend.split(' ')[1] === 'experience') {
+      const title =
+        legend.split(' ')[0] === 'educational'
+          ? 'Add education'
+          : 'Add position';
+
+      return (
+        <Icon
+          onClick={addMoreExperience}
+          className="add"
+          title={title}
+          path={mdiPlusBox}
+          size={1}
+        />
+      );
+    }
+  };
+
+  /**
+   * Clicking 'add' button forces a click on edit button
+   * and adds another experience component.
+   */
+  const addMoreExperience = () => {
+    let editButton;
+
+    if (legend.split(' ')[0] === 'educational') {
+      editButton = document.querySelector('[data-exp="educational"]');
+
+      addEducationalExpHandler({
+        school: '',
+        title: '',
+        date: '',
+      });
+    } else {
+      editButton = document.querySelector('[data-exp="practical"]');
+
+      addPracticalExpHandler({
+        company: '',
+        position: '',
+        responsibilities: '',
+        from: '',
+        to: '',
+      });
+    }
+
+    editButton.click();
+  };
+
   return (
     <fieldset>
       <legend>{legend}</legend>
       <div className="fields">{children}</div>
+      {displayAddButton()}
       <div className="buttons">
         <Button
           name="edit"
+          experience={legend.split(' ')[0]}
           buttonDisabled={editButtonDisabled}
           filled={filled}
           isEditing={isEditing}
