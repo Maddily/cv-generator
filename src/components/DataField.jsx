4e46c7c6f691d501bff7e958b0d9829f28c80876
Maddie <115709272+Maddily@users.noreply.github.com>
2024-12-08 10:29:14 +0200

    Document the component

--- src/components/DataField.jsx
+++ src/components/DataField.jsx
@@ -1,5 +1,12 @@
 import '../styles/DataField.css'
 
+/**
+ * A component that renders a label and its associated data.
+ *
+ * @param {string} label - A label for the data.
+ * @param {string} data - Data specific to this field.
+ * @returns {JSX.Element}
+ */
 export default function DataField({ label, data }) {
   return (
     <div className="data-field">
