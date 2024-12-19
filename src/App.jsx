b6c0d762ceaf45bebb6b26d79ab2681f5302064c
Maddie <115709272+Maddily@users.noreply.github.com>
2024-12-19 12:21:59 +0200

    Create a header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import './styles/App.css';
+import logo from './assets/images/logo.png';
 import Form from './components/Form';
 
 function App() {
@@ -26,24 +27,37 @@ function App() {
   ]);
 
   const handlePrinting = () => {
+    const header = document.querySelector('header');
+    header.style.display = 'none';
+
     const noPrint = document.querySelectorAll('.no-print');
     noPrint.forEach((element) => (element.style.display = 'none'));
 
     const experiences = document.querySelectorAll('.experience');
-    experiences.forEach((experience) => experience.style.marginTop = '-30px');
+    experiences.forEach((experience) => (experience.style.marginTop = '-30px'));
 
     const fields = document.querySelectorAll('.fields');
-    fields.forEach((fieldsElement) => fieldsElement.style.gap = '8px');
+    fields.forEach((fieldsElement) => (fieldsElement.style.gap = '8px'));
 
     window.print();
 
+    header.style.display = 'flex';
     noPrint.forEach((element) => (element.style.display = 'block'));
-    experiences.forEach((experience) => experience.style.marginTop = '0');
-    fields.forEach((fieldsElement) => fieldsElement.style.gap = '0');
+    experiences.forEach((experience) => (experience.style.marginTop = '0'));
+    fields.forEach((fieldsElement) => (fieldsElement.style.gap = '0'));
   };
 
   return (
     <>
+      <header>
+        <div className="logo-container">
+          <img className="logo" src={logo} alt="CV icon" />
+          <h1>CV Architect</h1>
+        </div>
+        <a className="generate-cv-link" href="#generate-cv">
+          Generate CV
+        </a>
+      </header>
       <Form
         isEditingGeneralInfo={isEditingGeneralInfo}
         isEditingEducationalExp={isEditingEducationalExp}
@@ -57,6 +71,7 @@ function App() {
         }
       />
       <button
+        id="generate-cv"
         disabled={!cvReady}
         onClick={handlePrinting}
         className="generate-cv no-print"
@@ -66,17 +81,26 @@ function App() {
       <footer className="no-print">
         <p>
           Licensed under{' '}
-          <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/" target='_blank'>
+          <a
+            href="https://creativecommons.org/licenses/by-nc-nd/4.0/"
+            target="_blank"
+          >
             CC BY-NC-ND 4.0
           </a>
         </p>
         <p>
           View the source code on{' '}
-          <a href="https://github.com/Maddily/cv-generator" target='_blank'>GitHub</a>
+          <a href="https://github.com/Maddily/cv-generator" target="_blank">
+            GitHub
+          </a>
         </p>
         <p>
           Favicon&nbsp;
-          <a href="https://www.flaticon.com/free-icons/cv" title="CV icons" target='_blank'>
+          <a
+            href="https://www.flaticon.com/free-icons/cv"
+            title="CV icons"
+            target="_blank"
+          >
             CV icon created by Freepik - Flaticon
           </a>
         </p>
